707a5f3856273d49578bc7099995f28da2cb0eea
hariompatil <hariompatil757@gmail.com>
2025-12-16 15:59:57 +0530

    Commit 7: Footer Component Build.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './Components/About/about.component';
-import { CakesComponent } from './Components/Cakes/cakes.component';
 import { OrderComponent } from './Components/Order/order.component';
 
 export const routes: Routes = [
-  { path: 'about', component: AboutComponent },
-  { path: 'cakes', component: CakesComponent },
-  { path: 'order', component: OrderComponent },
-  { path: '', redirectTo: 'about', pathMatch: 'full' }
+  {
+    path: 'about',
+    component: AboutComponent,
+    children: [{ path: 'order', component: OrderComponent }],
+  },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
 ];
