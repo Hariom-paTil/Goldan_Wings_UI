43b68558ee8bf79f1ce0f744c4128dedd9b7d77f
hariompatil <hariompatil757@gmail.com>
2026-02-03 18:13:38 +0530

    Commit 30: API fetch issue slowed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
     // Register AuthInterceptor
     { provide: HTTP_INTERCEPTORS, multi: true, useClass: AuthInterceptor },
   ],
